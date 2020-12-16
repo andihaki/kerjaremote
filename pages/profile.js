d6b718787ec26eb9bf743680b7b58426fdae5125
Andi Hakim Arif <andi.hakim.arif@gmail.com>
2020-12-16 21:45:52 +0700

    edit profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,16 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
 import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
 import auth0 from './api/utils/auth0';
 
-export default function Profile({ auth }) {
+export default function Profile({ auth, connected_user, user_profile }) {
   console.log({ auth });
-  const username = auth?.user?.nickname || auth?.user?.name || 'N.A';
-  const avatar = auth?.user?.picture
+
+  const { name, profilePicture, address = 'Alamat', profileDescription = 'Deskripsi dari diri anda', companyName = 'Nama Perusahaan', jobTitle = 'Pekerjaan', university = 'Nama Universitas' } = user_profile;
+  const username = name || auth?.user?.nickname || auth?.user?.name || 'N.A';
+  const avatar = profilePicture?.includes('http') ? profilePicture : auth?.user?.picture
   const isLogin = auth?.user?.nickname;
   const isRecruiter = auth?.user?.nickname?.includes('recruit') || auth?.user?.username?.includes('recruit')|| auth?.user?.username?.includes('name');
+
+  const friends = connected_user?.length;
+
+  // console.log({ connected_user, friends });
+  
+  console.log({ user_profile })
 
   return (
     <>
@@ -65,25 +74,34 @@ export default function Profile({ auth }) {
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
+                    <Link href={`/profile/edit`}>
+                      <button
+                        className="bg-gray-800 active:bg-gray-700 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+                        type="button"
+                      >
+                        <i className="fas fa-user-edit mr-2 text-lg text-gray-500"></i>{" "}
+                        Edit Profile
+                      </button>
+                    </Link>
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
                     <div className="flex justify-center py-4 lg:pt-4 pt-8">
                       <div className="mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          22
+                          {friends}
                         </span>
                         <span className="text-sm text-gray-500">Friends</span>
                       </div>
                       <div className="mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          10
+                          0
                         </span>
                         <span className="text-sm text-gray-500">Photos</span>
                       </div>
                       <div className="lg:mr-4 p-3 text-center">
                         <span className="text-xl font-bold block uppercase tracking-wide text-gray-700">
-                          89
+                          0
                         </span>
                         <span className="text-sm text-gray-500">Comments</span>
                       </div>
@@ -96,34 +114,30 @@ export default function Profile({ auth }) {
                   </h3>
                   <div className="text-sm leading-normal mt-0 mb-2 text-gray-500 font-bold uppercase">
                     <i className="fas fa-map-marker-alt mr-2 text-lg text-gray-500"></i>{" "}
-                    Los Angeles, California
+                    {address}
                   </div>
                   <div className="mb-2 text-gray-700 mt-10">
                     <i className="fas fa-briefcase mr-2 text-lg text-gray-500"></i>
-                    Solution Manager - Creative Tim Officer
+                    {jobTitle} - {companyName}
                   </div>
                   <div className="mb-2 text-gray-700">
                     <i className="fas fa-university mr-2 text-lg text-gray-500"></i>
-                    University of Computer Science
+                    {university}
                   </div>
                 </div>
                 <div className="mt-10 py-10 border-t border-gray-300 text-center">
                   <div className="flex flex-wrap justify-center">
                     <div className="w-full lg:w-9/12 px-4">
                       <p className="mb-4 text-lg leading-relaxed text-gray-800">
-                        An artist of considerable range, Jenna the name taken by
-                        Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                        performs and records all of his own music, giving it a
-                        warm, intimate feel with a solid groove structure. An
-                        artist of considerable range.
+                        {profileDescription}
                       </p>
-                      <a
+                      {/* <a
                         href="#pablo"
                         className="font-normal text-blue-500"
                         onClick={(e) => e.preventDefault()}
                       >
                         Show more
-                      </a>
+                      </a> */}
                     </div>
                   </div>
                 </div>
@@ -139,11 +153,42 @@ export default function Profile({ auth }) {
 
 export async function getServerSideProps(context) {
   const session = await auth0.getSession(context.req);
+
+  // connected user
+  const user_id = session.user.sub
+  const res_connected_user = await fetch(`${process.env.HOSTNAME}/api/db/connected-user`, {
+    method: 'POST',
+    body: {
+      keyword: user_id,
+    },
+    headers: {
+      authorization:
+        `Bearer ${process.env.AUTH0_TOKEN}`
+    }
+  });
+  const connected_user = await res_connected_user.json();
+
+  // user profile
+  const username = session.user.name;
+  const res_user_profile = await fetch(`${process.env.HOSTNAME}/api/db/user-profile`, {
+    method: 'POST',
+    body: {
+      keyword: username,
+    },
+    headers: {
+      authorization:
+        `Bearer ${process.env.AUTH0_TOKEN}`
+    }
+  });
+  const user_profile = await res_user_profile.json();
+  console.log({ username, user_profile })
   
   try {  
     return {
       props: {
-        auth: session
+        auth: session,
+        connected_user,
+        user_profile,
       },
     }
   } catch (err) {
